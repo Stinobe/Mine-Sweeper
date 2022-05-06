6b188790090bc05d00d7ce1b8290856659422349
Stinobe <stijn@vanwijmeersch.com>
2022-05-06 23:43:18 +0200

    Mine safety

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -6,11 +6,15 @@ import { useParams } from "react-router-dom";
 const GamePage: React.FC = () => {
   const params = useParams<{ x: string; y: string; mines: string }>();
 
-  const grid = new Grid(
-    Number(params.x),
-    Number(params.y),
-    Number(params.mines)
-  );
+  const minMines = Number(params.x) * Number(params.y) * 0.1;
+  const maxMines = Number(params.x) * Number(params.y) * 0.4;
+
+  let mines: number = Number(params.mines);
+
+  if (mines < minMines) mines = minMines;
+  if (mines > maxMines) mines = maxMines;
+
+  const grid = new Grid(Number(params.x), Number(params.y), Number(mines));
 
   return <Game tiles={grid.TileInfo} />;
 };
